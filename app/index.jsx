8dfb8a270acfc2187df968edad7d2290ca35dbd6
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2025-05-13 16:09:46 -0300

    refactor: Resolving conflicts in the index.jsx file

--- app/index.jsx
+++ app/index.jsx
@@ -7,8 +7,32 @@ import {
   TouchableOpacity,
 } from "react-native";
 import Banner from "../components/banner/Banner.js";
+import CategoriesCards from "../components/categoriesCards/CategoriesCards.js";
+import SearchBar from "../components/search/Search.js";
 
 export default function Home() {
+  const categories = [
+    {
+      title: "Cuidados Capilares",
+      description:
+        "Dicas e produtos para manter seus cabelos lindos e saudáveis",
+      route: "/cuidadosCapilares",
+    },
+    {
+      title: "Maquiagem",
+      description: "Dicas práticas para realçar sua beleza com maquiagem.",
+      route: "/maquiagem",
+    },
+    {
+      title: "Skin Care",
+      description: "Cuidados essenciais para uma pele bonita e bem tratada",
+      route: "/skinCare",
+    },
+  ];
+
+  const handleCategoryPress = (route) => {
+    router.push(route); // Navega para a rota correspondente
+  };
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
