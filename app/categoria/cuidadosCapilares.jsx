a7e4b957b0a513e86bb80c654e4b180089dd4ba4
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2025-05-30 17:31:48 -0300

    feat: finalization of the hair care page and cleaning up the code

--- app/categoria/cuidadosCapilares.jsx
+++ app/categoria/cuidadosCapilares.jsx
@@ -30,15 +30,12 @@ export default function Hair() {
       <Banner
         title="Cuidados Capilares"
         text="Bem-vindo à seção Cuidado Capilar do Lumina."
-        image={{
-          uri: "https://media.istockphoto.com/id/1024577076/pt/foto/full-frame-of-professional-hair-dresser-tools-on-white-background.jpg?s=170667a&w=0&k=20&c=xbXLKh6xn3iqWJ23tni9CuDlxxhKP89n6Y8UZCJ401U=",
-        }}
         gradientColors={["rgba(0, 0, 0, 0)", "rgb(167, 213, 236)"]}
       />
-      <View style={styles.containerLiso}>
-        <Text style={styles.titleLiso}>Cabelos Lisos</Text>
+      <View style={styles.containerCabelos}>
+        <Text style={styles.titleCabelos}>Cabelos Lisos</Text>
         <Image
-          style={styles.imageLiso}
+          style={styles.imageCabelos}
           source={{
             uri: "https://uploads.folhavitoria.com.br/2025/02/31iDrZV7-Copia-de-CORTE-DE-FOTOS-4-2-png.webp",
           }}
@@ -46,7 +43,7 @@ export default function Hair() {
         <Text style={styles.describe1}>
           Cabelos lisos são aqueles que possuem uma estrutura capilar reta, sem
           ondulações ou cachos. A textura do fio é suave e uniforme, sem curvas
-          ou frizz.{" "}
+          ou frizz.
         </Text>
       </View>
       <View style={styles.Dicas}>
@@ -60,66 +57,16 @@ export default function Hair() {
         <CustomButton title="Baixe PDF" />
       </View>
 
-      {/*-----------quimica--------------*/}
-
-      <View style={styles.containerQuimica}>
-        <Text style={styles.titleQuimica}>Cabelos com Química</Text>
+      <View style={styles.containerCabelos}>
+        <Text style={styles.titleCabelos}>Cabelos Cacheados</Text>
         <Image
-          style={styles.imageQuimica}
-          source={require("../../assets/quimica.png")}
-        />
-        <Text style={styles.describe1}>
-          Cabelos com química, referem-se a cabelos que passaram por processos
-          químicos para alterar sua estrutura ou cor.{"#"}
-        </Text>
-      </View>
-      <View style={styles.Dicas}>
-        <Text style={styles.titleDicas}>Dicas:</Text>
-        <Text style={styles.lineDicas}>__________________________________</Text>
-        <Text style={styles.describeDicas}>
-          Clique no botão abaixo para baixar nosso PDF exclusivo com dicas
-          essenciais para seu tipo ideal de cabelos contendo, produtos para
-          cronograma capilar e muito mais!
-        </Text>
-        <CustomButton title="Baixe PDF" />
-      </View>
-
-      {/*-----------mechas--------*/}
-      <View style={styles.containerMechas}>
-        <Text style={styles.titleMechas}>Cabelos com Mechas</Text>
-        <Image
-          style={styles.imageMechas}
-          source={require("../../assets/mechas.png")}
-        />
-
-        <Text style={styles.describe1}>
-          Cabelos com mechas, são aqueles que passam por um processo de
-          coloração onde algumas seções do cabelo recebem tons mais claros ou
-          mais escuros do que a cor natural dos fios.{" "}
-        </Text>
-      </View>
-      <View style={styles.Dicas}>
-        <Text style={styles.titleDicas}>Dicas:</Text>
-        <Text style={styles.lineDicas}>__________________________________</Text>
-        <Text style={styles.describeDicas}>
-          Clique no botão abaixo para baixar nosso PDF exclusivo com dicas
-          essenciais para seu tipo ideal de cabelos contendo, produtos para
-          cronograma capilar e muito mais!
-        </Text>
-        <CustomButton title="Baixe PDF" />
-      </View>
-
-      {/*-----------cachos--------*/}
-      <View style={styles.containerCachos}>
-        <Text style={styles.titleCachos}>Cabelos Cacheados</Text>
-        <Image
-          style={styles.imageCachos}
+          style={styles.imageCabelos}
           source={require("../../assets/cachos.png")}
         />
 
         <Text style={styles.describe1}>
           Cabelos cacheados são caracterizados por fios que formam cachos ou
-          ondas de forma natural.{" "}
+          ondas de forma natural.
         </Text>
       </View>
       <View style={styles.Dicas}>
@@ -133,40 +80,36 @@ export default function Hair() {
         <CustomButton title="Baixe PDF" />
       </View>
 
-      {/*-----------ondulados--------*/}
-      <View style={styles.containerOndulado}>
-        <Text style={styles.titleOndulado}>Cabelos Ondulados</Text>
+      <View style={styles.containerCabelos}>
+        <Text style={styles.titleCabelos}>Cabelos Crespos</Text>
         <Image
-          style={styles.imageOndulado}
-          source={require("../../assets/ondulado.png")}
-        />
-
-        <Text style={styles.describe1}>
-          Os cabelos ondulados, possuem uma textura que se situa entre os fios
-          lisos e os cacheados. Eles formam ondas suaves, com movimento natural.{" "}
-        </Text>
-      </View>
-      <View style={styles.Dicas}>
-        <Text style={styles.titleDicas}>Dicas:</Text>
-        <Text style={styles.lineDicas}>__________________________________</Text>
-        <Text style={styles.describeDicas}>
-          Clique no botão abaixo para baixar nosso PDF exclusivo com dicas
-          essenciais para seu tipo ideal de cabelos contendo, produtos para
-          cronograma capilar e muito mais!
-        </Text>
-        <CustomButton title="Baixe PDF" />
-      </View>
-
-      {/*-----------crespos--------*/}
-      <View style={styles.containerCrespo}>
-        <Text style={styles.titleCrespo}>Cabelos Crespos</Text>
-        <Image
-          style={styles.imageCrespo}
+          style={styles.imageCabelos}
           source={require("../../assets/crespo.png")}
         />
         <Text style={styles.describe1}>
           Cabelos crespos são caracterizados por fios que possuem uma curvatura
           muito apertada, o que resulta em um formato mais compacto e volumoso.{" "}
+        </Text>
+      </View>
+      <View style={styles.Dicas}>
+        <Text style={styles.titleDicas}>Dicas:</Text>
+        <Text style={styles.lineDicas}>__________________________________</Text>
+        <Text style={styles.describeDicas}>
+          Clique no botão abaixo para baixar nosso PDF exclusivo com dicas
+          essenciais para seu tipo ideal de cabelos contendo, produtos para
+          cronograma capilar e muito mais!
+        </Text>
+        <CustomButton title="Baixe PDF" />
+      </View>
+      <View style={styles.containerCabelos}>
+        <Text style={styles.titleCabelos}>Cabelos com Química</Text>
+        <Image
+          style={styles.imageCabelos}
+          source={require("../../assets/quimica.png")}
+        />
+        <Text style={styles.describe1}>
+          Cabelos com química, referem-se a cabelos que passaram por processos
+          químicos para alterar sua estrutura ou cor.{"#"}
         </Text>
       </View>
       <View style={styles.Dicas}>
@@ -188,16 +131,16 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "white",
   },
-  containerLiso: {
+  containerCabelos: {
     width: width * 0.9, // 90% da largura da tela
     margin: 10,
   },
-  titleLiso: {
+  titleCabelos: {
     fontSize: width * 0.05, // Tamanho da fonte baseado na largura da tela
     color: "#65a2bf",
-    marginTop: 20,
+    marginTop: 55,
   },
-  imageLiso: {
+  imageCabelos: {
     width: "100%", // Largura total do contêiner
     height: height * 0.2, // Altura em proporção à altura da tela
     borderRadius: 10,
@@ -216,7 +159,7 @@ const styles = StyleSheet.create({
   titleDicas: {
     fontSize: width * 0.05,
     color: "#65a2bf",
-    marginTop: 60,
+    marginTop: 10,
   },
   lineDicas: {
     fontSize: width * 0.05,
@@ -230,99 +173,5 @@ const styles = StyleSheet.create({
     marginTop: 15,
     width: "100%",
     textAlign: "justify",
-  },
-  // quimica
-
-  containerQuimica: {
-    width: width * 0.9, // 90% da largura da tela
-    margin: 10,
-  },
-  titleQuimica: {
-    fontSize: width * 0.05, // Tamanho da fonte baseado na largura da tela
-    color: "#65a2bf",
-    marginTop: 20,
-  },
-
-  imageQuimica: {
-    width: "100%", // Largura total do contêiner
-    height: height * 0.2, // Altura em proporção à altura da tela
-    borderRadius: 10,
-    marginTop: 20,
-  },
-
-  // mechas
-
-  containerMechas: {
-    width: width * 0.9, // 90% da largura da tela
-    margin: 10,
-  },
-  titleMechas: {
-    fontSize: width * 0.05, // Tamanho da fonte baseado na largura da tela
-    color: "#65a2bf",
-    marginTop: 20,
-  },
-
-  imageMechas: {
-    width: "100%", // Largura total do contêiner
-    height: height * 0.2, // Altura em proporção à altura da tela
-    borderRadius: 10,
-    marginTop: 20,
-  },
-
-  // cachos
-
-  containerCachos: {
-    width: width * 0.9, // 90% da largura da tela
-    margin: 10,
-  },
-  titleCachos: {
-    fontSize: width * 0.05, // Tamanho da fonte baseado na largura da tela
-    color: "#65a2bf",
-    marginTop: 20,
-  },
-
-  imageCachos: {
-    width: "100%", // Largura total do contêiner
-    height: height * 0.2, // Altura em proporção à altura da tela
-    borderRadius: 10,
-    marginTop: 20,
-  },
-
-  //ondulados
-
-  containerOndulado: {
-    width: width * 0.9, // 90% da largura da tela
-    margin: 10,
-  },
-  titleOndulado: {
-    fontSize: width * 0.05, // Tamanho da fonte baseado na largura da tela
-    color: "#65a2bf",
-    marginTop: 20,
-  },
-
-  imageOndulado: {
-    width: "100%", // Largura total do contêiner
-    height: height * 0.2, // Altura em proporção à altura da tela
-    borderRadius: 10,
-    marginTop: 20,
-  },
-
-  //crespos
-
-  containerCrespo: {
-    width: width * 0.9, // 90% da largura da tela
-    margin: 10,
-  },
-  titleCrespo: {
-    fontSize: width * 0.05, // Tamanho da fonte baseado na largura da tela
-    color: "#65a2bf",
-    marginTop: 20,
-  },
-
-  imageCrespo: {
-    width: "100%", // Largura total do contêiner
-    height: height * 0.2, // Altura em proporção à altura da tela
-    borderRadius: 10,
-    marginTop: 20,
   },
 });
